568664d94e87423758a6f40018e4741b487a4a2c
Rita Uzoma <you@example.com>
2022-04-14 09:19:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
+import styled from "@emotion/styled";
+import { Navbar } from "./components/Navbar";
+// import Button from "@mui/material/Button";
+// import image from "../src/images/bitcoin-toon.jpg";
+import { Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <App.Wrapper className="container">
+      <div className="initial-div">
+        <div className="second-div">
+          <Navbar />
+        </div>
+        <div className="hero-section">
+          <div className="description-section">
+            <h1>
+              Discover the digital art <br />
+              NFT marketplace
+            </h1>
+            <p>
+              Lorem Icons can convey all sorts of meaningful information, so
+              it's important to
+              <br /> ensure they are accessible where appropriate.
+            </p>
+            <div className="learn">
+              <button className="button-section">
+                <Link to="/">Explore</Link>
+              </button>
+              <h4 className="learn-more">
+                <Link to="/">Learn More</Link>
+              </h4>
+            </div>
+          </div>
+          <div className="image-section">
+            {/* <h3>nbkbf v</h3> */}
+            {/* <img src={image} alt="/" 
+              style={{
+                width: '100%',
+                marginRight: '20px'
+
+              }}
+            /> */}
+          </div>
+        </div>
+        <h2>Companies that trust Us</h2>
+        <div className="middle-section">
+          <p>Binance</p>
+          <p>BitMex</p>
+          <p>Blockport</p>
+          <p>Coinbase</p>
+        </div>
+      </div>
+    </App.Wrapper>
   );
 }
 
 export default App;
+
+App.Wrapper = styled.div`
+  @media and screen(max-width: 768px){
+    width: 100%;
+  }
+  .initial-div {
+    ${"" /* background: black; */}
+    margin: 0 100px;
+    height: 100vh;
+
+    .second-div {
+      border-bottom: 2px solid grey;
+    }
+
+    .hero-section {
+      display: flex;
+      flex-direction: row;
+      margin-top: 50px;
+
+      .description-section {
+        h1 {
+          font-size: 65px;
+          text-transform: capitalize;
+          line-height: 4rem;
+          font-family: "Rubik Moonrocks", cursive;
+          margin-top: 80px;
+          color: #545454;
+        }
+        p {
+          font-size: 20px;
+          font-family: cursive;
+          margin-bottom: 50px;
+          font-family: 'Rubik Glitch', cursive;
+          color: #545454;
+        }
+        .learn{
+          display: flex;
+          margin-bottom: 50px;
+          align-items: center;
+
+          .button-section{
+            margin-right: 35px;
+            padding: 15px 40px;
+          font-size: 20px;
+          font-weight: 700;
+          border: none;
+          color: #000;
+        background-color: #545454;
+        text-decoration: none;
+
+        a{
+          color: #000;
+        background-color: #545454;
+        text-decoration: none;
+        }
+          }
+          .learn-more a{
+            text-decoration: none;
+            font-size: 20px;
+            color:#545454;
+          }
+        }
+      }
+    }
+        .middle-section{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      ${'' /* gap: 5rem; */}
+      background:grey;
+      padding: 40px 30px;
+    
+      width: 100%;
+      p{
+        ${'' /* font-size: 30px; */}
+      font-family: 'Bungee', cursive;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      }
+    }
+    }
+  }
+
+`;
---
+++ src/components/Navbar.js
@@ -0,0 +1,129 @@
+import React, {useState} from "react";
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+// import SearchIcon from '@mui/icons-material/Search';
+
+// Using Async here because the data we're requesting here might take time
+async function getAccount(){
+  const accounts = await window.ethereum.request({method: "eth_requestAccounts"});
+  const account = accounts[0];
+  return account;
+}
+export function Navbar() {
+  const [addressAccount, setAddressAccount] = useState('')
+const connectWalletOnclick = () => {
+  if(window !== "undefined"){
+    getAccount().then((response) => {
+      setAddressAccount(response)
+    })
+  }
+}
+  return (
+    <Navbar.Wrapper className="navbar-container">
+      <nav className="navbar">
+        <h1>
+          <Link to="/" className="logo">
+            NFT.io
+          </Link>
+        </h1>
+        <ul className="unordered-nav">
+          <li className="list-nav">
+            <Link to="/">Discover</Link>
+            <Link to="/">Features</Link>
+            <Link to="/">Market</Link>
+            <Link to="/">Explore</Link>
+          </li>
+        </ul>
+        {/* <SearchIcon className="search-icon"/> */}
+
+        <div className="nav-buttons">
+          <Button variant="outlined" className="buttonss">
+            Log In
+          </Button>
+          <Button
+            variant="contained"
+            className="buttones"
+            onClick={connectWalletOnclick}
+          >
+            {!!addressAccount ? addressAccount : "Connect Wallet"}
+          </Button>
+        </div>
+      </nav>
+    </Navbar.Wrapper>
+  );
+}
+
+Navbar.Wrapper = styled.div`
+  padding: 30px 0;
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    .logo {
+      color: #545454;
+      text-decoration: none;
+      font-size: 30px;
+      font-family: "Rubik Moonrocks", cursive;
+      @media screen and (max-width: 1080px) {
+        font-size: 20px;
+      }
+    }
+
+    .unordered-nav {
+      display: flex;
+      justify-content: space-around;
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+
+      a {
+        text-decoration: none;
+        color: #545454;
+        display: inline-block;
+        padding: 0 15px;
+        font-size: 25px;
+        font-family: "Rubik Moonrocks", cursive;
+        @media screen and (max-width: 1080px) {
+          font-size: 15px;
+          padding: 0 5px;
+        }
+      }
+    }
+    .nav-buttons {
+      display: inline;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      .buttonss {
+        margin-right: 20px;
+        ${"" /* margin-left: -100px; */}
+        justify-content: space-between;
+        font-size: 15px;
+        color: #545454;
+        border: 1px solid #545454;
+        border-radius: 0;
+        font-family: "Rubik Moonrocks", cursive;
+        @media screen and (max-width: 1080px) {
+          font-size: 12px;
+          padding: 0 5px;
+        }
+      }
+      .buttones {
+        color: #000;
+        background-color: #545454;
+        border-radius: 0;
+        font-size: 15px;
+        font-family: "Rubik Moonrocks", cursive;
+
+        @media screen and (max-width: 1080px) {
+          font-size: 12px;
+          padding: 0 5px;
+        }
+      }
+    }
+  }
+`;
